d3e43cfaaa38f7a125c4ae5a3ac6e977936ba9db
Calvin Sykes <Calvin.Sykes@soton.ac.uk>
2023-12-14 23:23:32 +0000

    simplify day 14

--- day14.py
+++ day14.py
@@ -1,18 +1,13 @@
-from itertools import chain
-from tqdm import trange
-
 class D:
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
-    s = {NORTH: "N", EAST: "E", SOUTH: "S", WEST: "W"}
 
 def to_string(pattern):
     return "\n".join("".join(l) for l in pattern)
         
 def transpose(pattern):
-    # Make columns of pattern
     transposed = [list() for _ in range(len(pattern[0]))]
     for l in pattern:
         for i, c in enumerate(l):
@@ -24,22 +19,22 @@ def slide(pattern, reverse=False):
         pattern = [l[::-1] for l in pattern]
     
     slid_pattern = []
-    for col in pattern:
-        slid_col = []
+    for l in pattern:
+        slid = []
         # if ., continue
         # if #, mark position
         # if O, move forward to last_cube
         last_cube = -1
-        for i, c in enumerate(col):
+        for i, c in enumerate(l):
             if c == ".":
-                slid_col.append(c)
+                slid.append(c)
             elif c == "#":
-                slid_col.append(c)
+                slid.append(c)
                 last_cube = i
             elif c == "O":
-                slid_col.insert(last_cube+1, c)
+                slid.insert(last_cube + 1, c)
                 last_cube += 1
-        slid_pattern.append(slid_col)
+        slid_pattern.append(slid)
 
     if reverse:
         slid_pattern = [l[::-1] for l in slid_pattern]
@@ -71,40 +66,36 @@ def load(pattern):
 
 def day14_part1(filename):
     with open(filename) as f:
-        lines = f.readlines()
-        lines = [l.rstrip("\n") for l in lines] # remove newlines
+        pattern = [l.rstrip("\n") for l in f.readlines()] # remove newlines
 
-    tilted = tilt(lines, D.NORTH)
+    tilted = tilt(pattern, D.NORTH)
     return load(tilted)
 
 def day14_part2(filename):
     with open(filename) as f:
-        lines = f.readlines()
-        lines = [l.rstrip("\n") for l in lines] # remove newlines
+        pattern = [l.rstrip("\n") for l in f.readlines()] # remove newlines
 
     it = 0
-    spun = lines
     cache = {}
-    cycle_length = -1
-    cycle_start = -1
-    N = 1000000000
+    cycle_length = None
     while True:
-        spun = spin_cycle(spun)
-        it += 1
-        
-        if cycle_length == -1:
-            pstr = to_string(spun)
+        pattern = spin_cycle(pattern)
+        if not cycle_length:
+            pstr = to_string(pattern)
             if pstr in cache:
                 cycle_start = it
                 cycle_length = it - cache[pstr]
                 break
             else:
                 cache[pstr] = it
-    
-    reverse_cache = {v: k for k, v in cache.items()}
+        it += 1
+
+    N = 1000000000
     offset = cycle_start - cycle_length
-    index = (N - offset) % cycle_length + offset
-    return load(reverse_cache[index].split("\n"))
+    index = (N - 1 - offset) % cycle_length + offset
+    for pstr, it in cache.items():
+        if it == index:
+            return load(pstr.split("\n"))
 
 if __name__ == "__main__":
     print("Part 1 example", day14_part1("input/day14_example.txt"))
